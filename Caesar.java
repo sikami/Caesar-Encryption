4d286fc0361743e963031c2647b66075c19c31e3
sikami <widyasari.listya@gmail.com>
2020-08-03 17:46:30 +0100

    verify key is used to validate this.key

--- Caesar.java
+++ Caesar.java
@@ -2,15 +2,69 @@ package com.caesar;
 
 import java.util.Scanner;
 
+import static java.lang.Character.isLowerCase;
+import static java.lang.Character.isUpperCase;
+
 public class Caesar {
 
-    //key
-    private Scanner scanner;
+    private int key;
+    private String text;
 
-    private int key(int key) {
-        while(key > 26) {
-            key -= 25;
+    public Caesar(int key) {
+        this.key = (key > 26)? verifyKey(): key;
+    }
+
+    public Caesar(String text) {
+        this.text = text;
+    }
+
+    public Caesar(int key, String text) {
+        this.key = key;
+        this.text = text;
+    }
+
+    public Caesar() {
+    }
+
+    private int verifyKey() {
+        while(this.key > 26) {
+            this.key -= 25;
         }
-        return key;
+        return this.key;
+    }
+
+    public String encrypt(String text) {
+        int key = verifyKey();
+
+        char result = 0;
+        String texts = null;
+        for(char i = 0; i < text.length(); i++) {
+            result = convert(i);
+        }
+        texts += result;
+        return texts;
+        
+    }
+    private char convert(char word) {
+        int ascii;
+         if (isUpperCase(word)) {
+             if(word + this.key > 'Z') {
+                 ascii = word + this.key - 26;
+                 return (char) ascii;
+             } else {
+                 ascii = word + this.key;
+                 return (char) ascii;
+             }
+          } else if (isLowerCase(word)) {
+             if(word +this.key > 'z') {
+                 ascii = word + this.key - 26;
+                 return (char) ascii;
+             } else {
+                 ascii = word + this.key;
+                 return (char) ascii;
+             }
+         } else {
+             return word;
+         }
     }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package com.caesar;
 
 public class Main {
     public static void main(String[] args) {
-
+        Caesar caesar = new Caesar(3);
+        caesar.getClass()
     }
 }
